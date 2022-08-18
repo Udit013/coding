563816b5259d8c89f345d5d519c5d878d0d060e0
Udit Agarwal <agarwaludit13@gmail.com>
2022-08-18 21:11:01 +0530

    final commit

--- DAA-Lab/LAB4_12-08-2022/SelectionSort.cpp
+++ DAA-Lab/LAB4_12-08-2022/SelectionSort.cpp
@@ -13,7 +13,7 @@ void swap(int &x, int &y)
 
 int selectionSort(int arr[], int n)
 {
-    int c=0;
+    int c = 0;
     int min;
     for (int i = 0; i < n - 1; i++)
     {
@@ -31,29 +31,35 @@ int selectionSort(int arr[], int n)
     return c;
 }
 
-int optimizedSelectionSort(int arr[], int n){
-    int c=0;
-    for(int i=0, j=n-1; i<j; i++, j--){
+int optimizedSelectionSort(int arr[], int n)
+{
+    int c = 0;
+    for (int i = 0, j = n - 1; i < j; i++, j--)
+    {
         c++;
-        int min=i; int max=i;
-        int min_index=i; int max_index=i;   
-        for(int k=i; k<=j; k++){
+        int min = i;
+        int max = i;
+        int min_index = i;
+        int max_index = i;
+        for (int k = i; k <= j; k++)
+        {
             c++;
-            if(arr[k]>max)
+            if (arr[k] > max)
             {
-                max=arr[k];
-                max_index=k;
+                max = arr[k];
+                max_index = k;
             }
-            else if(arr[k]<max){
-                min=arr[k];
-                min_index=k;
+            else if (arr[k] < max)
+            {
+                min = arr[k];
+                min_index = k;
             }
         }
         swap(arr[i], arr[min_index]);
-        if(arr[min_index]==max)
-        swap(arr[j], arr[min_index]);
+        if (arr[min_index] == max)
+            swap(arr[j], arr[min_index]);
         else
-        swap(arr[max_index], arr[j]);
+            swap(arr[max_index], arr[j]);
     }
     return c;
 }
@@ -68,17 +74,17 @@ void display()
         int arr1[n];
         for (int i = 0; i < n; i++)
         {
-            arr1[i] = i+1;
+            arr1[i] = i + 1;
         }
         int bestCase = selectionSort(arr1, n);
         int arr2[n];
-        for (int i = n; i >=1; i--)
+        for (int i = n; i >= 1; i--)
         {
             arr2[i] = i;
         }
         int worstCase = selectionSort(arr2, n);
-        
-        cout << ++sl << " \t " << n << "\t\t" << bestCase<< "\t\t"<< worstCase<< "\n";
+
+        cout << ++sl << " \t " << n << "\t\t" << bestCase << "\t\t" << worstCase << "\n";
     }
 }
 
@@ -92,24 +98,23 @@ void displayOptimized()
         int arr1[n];
         for (int i = 0; i < n; i++)
         {
-            arr1[i] = i+1;
+            arr1[i] = i + 1;
         }
         int bestCase = optimizedSelectionSort(arr1, n);
         int arr2[n];
-        for (int i = n; i >=1; i--)
+        for (int i = n; i >= 1; i--)
         {
             arr2[i] = i;
         }
         int worstCase = optimizedSelectionSort(arr2, n);
-        
-        cout << ++sl << " \t " << n << "\t\t" << bestCase<< "\t\t"<< worstCase<< "\n";
+
+        cout << ++sl << " \t " << n << "\t\t" << bestCase << "\t\t" << worstCase << "\n";
     }
 }
 int main()
 {
-    cout<<"\n Selection sort: \n";
+    cout << "\n Selection sort: \n";
     display();
-    cout<<"\n Optimised selection sort \n";
+    cout << "\n Optimised selection sort \n";
     displayOptimized();
-
 }
