50f0cb304941e584fd29b48b58e4c530de1712f1
Udit Agarwal <agarwaludit13@gmail.com>
2022-08-21 01:12:10 +0530

    regular commit

--- CN-Lab/Lab4_17-08-2022/udp_client.c
+++ CN-Lab/Lab4_17-08-2022/udp_client.c
@@ -14,27 +14,29 @@
 #define SERVERPORT 4952 // the port users will be connecting to
 int main()
 {
-int sockfd;
-struct sockaddr_in their_addr; // connector's address information
-//struct hostent *he;
-int numbytes;
-char arg[30];
-if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-perror("socket");
-exit(1);
-}
-their_addr.sin_family = AF_INET; // host byte order
-their_addr.sin_port = htons(SERVERPORT); // short, network byte order
-their_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-//memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
-printf("Enter a message\n");
-gets(arg);
-if ((numbytes = sendto(sockfd, arg, strlen(arg), 0,
-(struct sockaddr *)&their_addr, sizeof their_addr)) == -1) {
-perror("sendto");
-exit(1);
-}
-printf("sent %d bytes to %s\n", numbytes, inet_ntoa(their_addr.sin_addr));
-close(sockfd);
-return 0;
+    int sockfd;
+    struct sockaddr_in their_addr; // connector's address information
+    // struct hostent *he;
+    int numbytes;
+    char arg[30];
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+    their_addr.sin_family = AF_INET;         // host byte order
+    their_addr.sin_port = htons(SERVERPORT); // short, network byte order
+    their_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
+    printf("Enter a message\n");
+    gets(arg);
+    if ((numbytes = sendto(sockfd, arg, strlen(arg), 0,
+                           (struct sockaddr *)&their_addr, sizeof their_addr)) == -1)
+    {
+        perror("sendto");
+        exit(1);
+    }
+    printf("sent %d bytes to %s\n", numbytes, inet_ntoa(their_addr.sin_addr));
+    close(sockfd);
+    return 0;
 }
