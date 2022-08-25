41ffb2be8e2f657cf9b5fbf184746457a45fea96
Udit013 <agarwaludit13@gmail.com>
2022-08-25 20:59:07 +0530

    regular commit

---
+++ CN-Lab/Lab5_24-08-2022/client.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define SERVERPORT 4952
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in their_addr;
+
+    int numbytes;
+    int num1, num2;
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+    their_addr.sin_family = AF_INET;
+    their_addr.sin_port = htons(SERVERPORT);
+    their_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    printf("Enter a message\n");
+    scanf("%d", num1);
+    if (numbytes = write(sockfd, &num1, sizeof(num1)) == -1)
+    {
+        perror("sendto");
+        exit(1);
+    }
+    printf("sent %d bytes to %s\n", numbytes, inet_ntoa(their_addr.sin_addr));
+    close(sockfd);
+    return 0;
+}
--- DAA-Lab/LAB5_19-08-2022/MergeSort.cpp
+++ DAA-Lab/LAB5_19-08-2022/MergeSort.cpp
@@ -1,33 +1,33 @@
-// Merge sort in C++
+/* 1. Write a program to implement Merge Sort and compare the complexities
+ for best, worst and average cases. Also, plot a graph for the same. */
 
 #include <iostream>
 using namespace std;
 
-// Merge two subarrays L and M into arr
-void merge(int arr[], int p, int q, int r)
+void merge(int arr[], int p, int q, int r, int &count)
 {
 
-  // Create L ← A[p..q] and M ← A[q+1..r]
   int n1 = q - p + 1;
   int n2 = r - q;
-
   int L[n1], M[n2];
-
   for (int i = 0; i < n1; i++)
+  {
+    count++;
     L[i] = arr[p + i];
+  }
   for (int j = 0; j < n2; j++)
+  {
+    count++;
     M[j] = arr[q + 1 + j];
-
-  // Maintain current index of sub-arrays and main array
+  }
   int i, j, k;
   i = 0;
   j = 0;
   k = p;
 
-  // Until we reach either end of either L or M, pick larger among
-  // elements L and M and place them in the correct position at A[p..r]
   while (i < n1 && j < n2)
   {
+    count++;
     if (L[i] <= M[j])
     {
       arr[k] = L[i];
@@ -40,11 +40,9 @@ void merge(int arr[], int p, int q, int r)
     }
     k++;
   }
-
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
   while (i < n1)
   {
+    count++;
     arr[k] = L[i];
     i++;
     k++;
@@ -52,45 +50,59 @@ void merge(int arr[], int p, int q, int r)
 
   while (j < n2)
   {
+    count++;
     arr[k] = M[j];
     j++;
     k++;
   }
 }
 
-// Divide the array into two subarrays, sort them and merge them
-void mergeSort(int arr[], int l, int r)
+int mergeSort(int arr[], int l, int r)
 {
+  int count = 0;
   if (l < r)
   {
-    // m is the point where the array is divided into two subarrays
+    count++;
     int m = l + (r - l) / 2;
-
     mergeSort(arr, l, m);
     mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r, count);
+  }
+  return count;
+}
 
-    // Merge the sorted subarrays
-    merge(arr, l, m, r);
+void printArray()
+{
+  srand(time(0));
+  int sl = 0;
+  cout << "Sl. No \t  Value of n \t TC(Best case)    TC(Worst Case)    TC(Avg Case)  \n\n";
+  for (int n = 500; n <= 5000; n = n + 500)
+  {
+    int arr1[n];
+    for (int i = 0; i < n; i++)
+    {
+      arr1[i] = i + 1;
+    }
+    int bestCase = mergeSort(arr1, 0, n - 1);
+    int arr2[n];
+    for (int i = n; i >= 1; i--)
+    {
+      arr2[i] = i;
+    }
+    int worstCase = mergeSort(arr2, 0, n - 1);
+    int arr3[n];
+    for (int i = n; i >= 1; i--)
+    {
+      arr3[i] = rand() % 10;
+    }
+    int avgcase = mergeSort(arr3, 0, n - 1);
+
+    cout << ++sl << " \t " << n << "\t\t" << bestCase << "\t\t" << worstCase << "\t\t\t" << avgcase << "\n";
   }
 }
 
-// Print the array
-void printArray(int arr[], int size)
-{
-  for (int i = 0; i < size; i++)
-    cout << arr[i] << " ";
-  cout << endl;
-}
-
-// Driver program
 int main()
 {
-  int arr[] = {6, 5, 12, 10, 9, 1};
-  int size = sizeof(arr) / sizeof(arr[0]);
-
-  mergeSort(arr, 0, size - 1);
-
-  cout << "Sorted array: \n";
-  printArray(arr, size);
+  printArray();
   return 0;
 }
